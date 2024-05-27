38e77f9ab6c3830015e1207dd130d9d7a1d11d5d
Klajdi Murataj <klajdimurati3@gmail.com>
2024-05-27 23:56:54 +0200

    added seed data

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,22 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occured during migration");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -3,19 +3,19 @@
     public class Product : BaseEntity
     { 
     
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
-        public string? Description { get; set; }
+        public string Description { get; set; }
 
-        public string? Price { get; set; }
+        public decimal? Price { get; set; }
 
-        public string? PictureUrl { get; set; }
+        public string PictureUrl { get; set; }
 
-        public ProductType? ProductType { get; set; }
+        public ProductType ProductType { get; set; }
 
         public int ProductTypeId { get; set; }
 
-        public ProductBrand? ProductBrand { get; set; }
+        public ProductBrand ProductBrand { get; set; }
 
         public int ProductBrandId { get; set; }
 
