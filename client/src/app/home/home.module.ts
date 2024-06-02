d8d8cd48c935a077577acf2dc4002fce6b23caf8
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-03 00:08:19 +0200

    added routing

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from '../home.component';
-
-
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}
