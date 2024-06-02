12490fecd899d183ebefbc61b01d4fc04e0e429c
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-02 20:12:29 +0200

    implemented filters

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../shared/models/product.model';
 import { ShopService } from './shop.service';
+import { IBrand } from '../shared/models/brand.model';
+import { IProductType } from '../shared/models/product-type.model';
 
 @Component({
   selector: 'app-shop',
@@ -9,15 +11,38 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[]=[];
+  brands: IBrand[]=[];
+  productTypes: IProductType[]=[];
 
   constructor(private _shopService: ShopService){}
 
   ngOnInit(): void {
+    this.getBrands();
+    this.getProductTypes();
+    this.getProducts();
+  }
+
+  getProducts(){
     this._shopService.getProducts().subscribe(response => {
-      this.products = response.data
+      this.products = response.data;
     }, error => {
       console.log(error);
     });
   }
 
+  getBrands(){
+    this._shopService.getBrands().subscribe(response => {
+      this.brands = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getProductTypes(){
+    this._shopService.getProductTypes().subscribe(response => {
+      this.productTypes = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
