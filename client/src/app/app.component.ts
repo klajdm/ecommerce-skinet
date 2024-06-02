0db5ee79cca3a0757e409a6ea82af81a026ac857
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-02 15:58:32 +0200

    added navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'Skinet';
+
+  products: IProduct[];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    
+  }
 }
