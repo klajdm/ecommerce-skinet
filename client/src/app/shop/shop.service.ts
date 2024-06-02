00b144c3b08a7ce599aacb5f9df61275de2637ef
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-02 20:04:51 +0200

    added models and services

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPagination } from '../shared/models/pagination.model';
+import { IBrand } from '../shared/models/brand.model';
+import { IProductType } from '../shared/models/product-type.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,13 @@ export class ShopService {
 
   getProducts(){
     return this.http.get<IPagination>(this.baseUrl + 'products');
+  }
+
+  getBrands(){
+    return this.http.get<IBrand>(this.baseUrl + 'products/brands');
+  }
+
+  getProductTypes(){
+    return this.http.get<IProductType>(this.baseUrl + 'products/types');
   }
 }
