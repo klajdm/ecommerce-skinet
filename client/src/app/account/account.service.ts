cd70817a9e840e66a05aad8f53de10683fbc851a
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-08 01:37:30 +0200

    added new endpoints and data model

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -4,6 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, map, of, ReplaySubject } from 'rxjs';
 import { IUser } from '../shared/models/user.mode';
 import { Router } from '@angular/router';
+import { IAddress } from '../shared/models/address.model';
 
 @Injectable({
   providedIn: 'root',
@@ -64,5 +65,13 @@ export class AccountService {
 
   checkEmailExists(email: string) {
     return this.http.get(this.baseUrl + 'account/emailexists?email=' + email);
+  }
+
+  getUserAddress() {
+    return this.http.get<IAddress>(this.baseUrl + 'account/address');
+  }
+
+  updateUserAddress(address: IAddress) {
+    return this.http.put<IAddress>(this.baseUrl + 'account/address', address);
   }
 }
--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -9,6 +9,7 @@ import {
   IBasketTotals,
 } from '../shared/models/basket.model';
 import { IProduct } from '../shared/models/product.model';
+import { IDeliveryMethod } from '../shared/models/delivery.model';
 
 @Injectable({
   providedIn: 'root',
@@ -22,7 +23,14 @@ export class BasketService {
   private basketTotalSource = new BehaviorSubject<IBasketTotals | null>(null);
   basketTotal$ = this.basketTotalSource.asObservable();
 
+  shipping = 0;
+
   constructor(private http: HttpClient) {}
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod) {
+    this.shipping = deliveryMethod.price;
+    this.calculateTotals();
+  }
 
   getBasket(id: string) {
     return this.http.get<IBasket>(this.baseUrl + 'basket?id=' + id).pipe(
@@ -86,6 +94,12 @@ export class BasketService {
     }
   }
 
+  deleteLocalBasket(id: string) {
+    this.basketSource.next(null);
+    this.basketTotalSource.next(null);
+    localStorage.removeItem('basket_id');
+  }
+
   deleteBasket(basket: IBasket) {
     return this.http
       .delete(this.baseUrl + 'basket?id=' + basket.id)
@@ -98,7 +112,7 @@ export class BasketService {
 
   private calculateTotals() {
     const basket = this.getCurrentBasketValue();
-    const shipping = 0;
+    const shipping = this.shipping;
     const subtotal = basket!.items.reduce(
       (a, b) => b.price * b.quantity + a,
       0
