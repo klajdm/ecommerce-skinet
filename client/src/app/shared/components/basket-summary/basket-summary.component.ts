9dd8417c57176b35841db4623cdc3a26289592dd
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-08 00:20:18 +0200

    created shared basket component

---
+++ client/src/app/shared/components/basket-summary/basket-summary.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BasketService } from '../../../basket/basket.service';
+import { BasketItem, IBasket } from '../../models/basket.model';
+
+@Component({
+  selector: 'app-basket-summary',
+  templateUrl: './basket-summary.component.html',
+  styleUrls: ['./basket-summary.component.scss'],
+})
+export class BasketSummaryComponent implements OnInit {
+  basket$: Observable<IBasket | null> | undefined;
+  @Output() decrement: EventEmitter<BasketItem> =
+    new EventEmitter<BasketItem>();
+  @Output() increment: EventEmitter<BasketItem> =
+    new EventEmitter<BasketItem>();
+  @Output() remove: EventEmitter<BasketItem> = new EventEmitter<BasketItem>();
+
+  constructor(private basketService: BasketService) {}
+
+  ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
+  }
+
+  removeBasketItem(item: BasketItem) {
+    this.remove.emit(item);
+  }
+
+  incrementItemQuantity(item: BasketItem) {
+    this.increment.emit(item);
+  }
+
+  decrementItemQuantity(item: BasketItem) {
+    this.decrement.emit(item);
+  }
+}
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -10,6 +10,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { CdkStepperModule } from '@angular/cdk/stepper';
 import { StepperComponent } from './components/stepper/stepper.component';
+import { BasketSummaryComponent } from './components/basket-summary/basket-summary.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { StepperComponent } from './components/stepper/stepper.component';
     OrderTotalsComponent,
     TextInputComponent,
     StepperComponent,
+    BasketSummaryComponent,
   ],
   imports: [
     CommonModule,
@@ -26,6 +29,7 @@ import { StepperComponent } from './components/stepper/stepper.component';
     CarouselModule.forRoot(),
     BsDropdownModule.forRoot(),
     CdkStepperModule,
+    RouterModule,
   ],
   exports: [
     PaginationModule,
@@ -33,11 +37,13 @@ import { StepperComponent } from './components/stepper/stepper.component';
     PagerComponent,
     TextInputComponent,
     StepperComponent,
+    BasketSummaryComponent,
     CarouselModule,
     OrderTotalsComponent,
     ReactiveFormsModule,
     BsDropdownModule,
     CdkStepperModule,
+    RouterModule,
   ],
 })
 export class SharedModule {}
