67275dd992f574d129b6eec83743130829d80c50
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-08 16:51:00 +0200

    buildup orders module

---
+++ client/src/app/orders/order-detailed/order-detailed.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { IOrder } from '../../shared/models/order.model';
+import { OrdersService } from '../orders.service';
+import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
+
+@Component({
+  selector: 'app-order-detailed',
+  templateUrl: './order-detailed.component.html',
+  styleUrls: ['./order-detailed.component.scss'],
+})
+export class OrderDetailedComponent implements OnInit {
+  order: IOrder;
+
+  constructor(
+    private ordersService: OrdersService,
+    private route: ActivatedRoute,
+    private breadcrumbService: BreadcrumbService
+  ) {}
+
+  ngOnInit(): void {
+    this.getOrderDetailed();
+  }
+
+  getOrderDetailed() {
+    this.ordersService
+      .getOrderDetailed(+this.route.snapshot.paramMap.get('id'))
+      .subscribe(
+        (order: IOrder) => {
+          this.order = order;
+          this.breadcrumbService.set(
+            '@OrderDetailed',
+            `Order# ${order.id} - ${order.status}`
+          );
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+}
