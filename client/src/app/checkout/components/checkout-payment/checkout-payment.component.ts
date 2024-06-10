c64c98621ea503a19841e0ebe565a3a5c488269c
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-10 13:34:45 +0200

    updated payment validations

--- client/src/app/checkout/components/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/components/checkout-payment/checkout-payment.component.ts
@@ -33,6 +33,9 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
   cardErrors: any;
   cardHandler = this.onChange.bind(this);
   loading = false;
+  cardNumberValid = false;
+  cardExpiryValid = false;
+  cardCvcValid = false;
 
   constructor(
     private checkoutService: CheckoutService,
@@ -66,11 +69,23 @@ export class CheckoutPaymentComponent implements AfterViewInit, OnDestroy {
     this.cardCvc.destroy();
   }
 
-  onChange({ error }) {
-    if (error) {
-      this.cardErrors = error.message;
+  onChange(event) {
+    if (event.error) {
+      this.cardErrors = event.error.message;
     } else {
       this.cardErrors = null;
+    }
+
+    switch (event.elementType) {
+      case 'cardNumber':
+        this.cardNumberValid = event.complete;
+        break;
+      case 'cardExpiry':
+        this.cardExpiryValid = event.complete;
+        break;
+      case 'cardCvc':
+        this.cardCvcValid = event.complete;
+        break;
     }
   }
 
