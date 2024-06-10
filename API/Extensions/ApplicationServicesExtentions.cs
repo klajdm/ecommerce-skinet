1d5d176d65c46f310edd3dedc00c6fd25f7fe59e
Klajdi Murataj <klajdimurati3@gmail.com>
2024-06-10 22:37:11 +0200

    implemented API cache

--- API/Extensions/ApplicationServicesExtentions.cs
+++ API/Extensions/ApplicationServicesExtentions.cs
@@ -10,6 +10,7 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
+            services.AddSingleton<IResponseCacheService, ResponseCacheService>();
             services.AddScoped<ITokenService, TokeService>();
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IPaymentService, PaymentService>();
